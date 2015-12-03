c7dd301f03bb168c33625b6e570332db764ba351
Carl Venezia <veneziacarl@gmail.com>
2015-12-03 14:20:04 -0500

    added infinite scrolling feature

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -1,20 +1,27 @@
-$('.more-sprouts').click(function(event){
-  event.preventDefault();
-  var updatePageNumber = function(){
-    var $link = $('a.more-sprouts');
-    var pageNumber = $link.attr('href').match(/\d+$/);
-    $link.attr('href', '/tweets?page=' + (Number(pageNumber) + 1));
-    return Number(pageNumber);
-  };
+$(window).scroll(function() {
+  if($(window).scrollTop() + $(window).height() > $(document).height()  - 200) {
 
-  $.ajax({
-    method: "GET",
-    url: "/tweets.json",
-    data: {page: updatePageNumber()},
-    success: function(data){
-      data.forEach(function(tweet){
-        $('ul.tweets').append('<li class="tweet"><div class="body">' + tweet.text + '</div><div class="user">' + tweet.username + '</div></li>');
+    $('a.more-sprouts').hide();
+
+    // $('.more-sprouts').click(function(event){
+    //   event.preventDefault();
+      var updatePageNumber = function(){
+        var $link = $('a.more-sprouts');
+        var pageNumber = $link.attr('href').match(/\d+$/);
+        $link.attr('href', '/tweets?page=' + (Number(pageNumber) + 1));
+        return Number(pageNumber);
+      };
+
+      $.ajax({
+        method: "GET",
+        url: "/tweets.json",
+        data: {page: updatePageNumber()},
+        success: function(data){
+          data.forEach(function(tweet){
+            $('ul.tweets').append('<li class="tweet"><div class="body">' + tweet.text + '</div><div class="user">' + tweet.username + '</div></li>');
+          });
+        }
       });
-    }
-  });
+    // });
+  }
 });
