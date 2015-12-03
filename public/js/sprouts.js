86a0a85a0221640ac9795380c946ed68761b6ef4
Carl Venezia <veneziacarl@gmail.com>
2015-12-03 13:07:45 -0500

    completed required challenge

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -1,1 +1,20 @@
-// YOUR CODE GOES HERE
+$('.more-sprouts').click(function(event){
+  event.preventDefault();
+  var updatePageNumber = function(){
+    var $link = $('a.more-sprouts');
+    var pageNumber = $link.attr('href').match(/\d+$/);
+    $link.attr('href', '/tweets?page=' + (Number(pageNumber) + 1));
+    return Number(pageNumber);
+  };
+
+  $.ajax({
+    method: "GET",
+    url: "/tweets.json",
+    data: {page: updatePageNumber()},
+    success: function(data){
+      data.forEach(function(tweet){
+        $('ul.tweets').append('<li class="tweet"><div class="body">' + tweet.text + '</div><div class="user">' + tweet.username + '</div></li>');
+      });
+    }
+  });
+});
